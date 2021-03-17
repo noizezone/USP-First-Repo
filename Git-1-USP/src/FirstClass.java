ff157ab44662ef8c1fc808e6fd3187c03627c2a1
Stanislav <lazarov142@gmail.com>
2021-03-18 00:55:14 +0200

    First Push

--- Git-1-USP/src/FirstClass.java
+++ Git-1-USP/src/FirstClass.java
@@ -4,8 +4,10 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
 		System.out.println("First Repo!");
+		for(int i = 0; i < 10; i++) {
+			System.out.print(i);
+		}
 	}
 
 }
