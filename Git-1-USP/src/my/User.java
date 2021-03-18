106bb370b657cd2e0e2a5aacaca69a1477a27559
Stanislav <lazarov142@gmail.com>
2021-03-18 09:34:42 +0200

    Changes name to myname

---
+++ Git-1-USP/src/my/User.java
@@ -0,0 +1,14 @@
+package my;
+
+public class User {
+	
+	public String myName;
+	
+	public String getName() {
+		return myName;
+	}
+	
+	public void setName(String name) {
+		this.myName = name;
+	}
+}
